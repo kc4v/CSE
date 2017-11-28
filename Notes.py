e89ba635e9d8bc5568623eb2caeb3df97ff8207e
kc4v <33762606+kc4v@users.noreply.github.com>
2017-11-28 10:46:50 -0800

    Added previous notes
    11-27-17

--- Notes.py
+++ Notes.py
@@ -1,8 +1,29 @@
-# This is working
+
+# Trenten Williams
+# (This is python 2.7)
+
+print(3 + 5)
+print(3 - 5)
+print(3 * 5)
+print(6 / 2)
+print(3 ** 5)
 
 
+print("See if you can figure this out")
+print(5 % 3)
 
 
+car_name: 'Shadow Car'
+car_type = 'Lamborghini Sesto Elemento'
+car_cylinders = 8
+car_mpg = 9000.1
+
+# Inline printing
+print("I have a car called the %s. It's a %s." % (car_name, car_type))
 
 
+name = input("What is your name? ")
+print("Hello %s." % name)
 
+age = input("How old are you? ")
+print("Wow %s, That's pretty old." % age)
